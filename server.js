41c3b04a83932df1c5a30c7378f0246c55ae393b
logeshloki585 <Logeshtv21@gmail.com>
2024-07-28 20:09:00 +0530

    cors updated

--- server.js
+++ server.js
@@ -1,28 +1,39 @@
-const express = require("express")
-const http = require("http")
-const app = express()
-const server = http.createServer(app)
+const express = require("express");
+const http = require("http");
+const cors = require("cors");
+const app = express();
+const server = http.createServer(app);
+
 const io = require("socket.io")(server, {
-	cors: {
-		origin: "https://dynamic-youtiao-6e1105.netlify.app",
-		methods: [ "GET", "POST" ]
-	}
-})
+    cors: {
+        origin: "https://dynamic-youtiao-6e1105.netlify.app",
+        methods: ["GET", "POST"],
+        allowedHeaders: ["my-custom-header"],
+        credentials: true
+    }
+});
+
+app.use(cors({
+    origin: "https://dynamic-youtiao-6e1105.netlify.app",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true
+}));
 
 io.on("connection", (socket) => {
-	socket.emit("me", socket.id)
+    socket.emit("me", socket.id);
 
-	socket.on("disconnect", () => {
-		socket.broadcast.emit("callEnded")
-	})
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded");
+    });
 
-	socket.on("callUser", (data) => {
-		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
-	})
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name });
+    });
 
-	socket.on("answerCall", (data) => {
-		io.to(data.to).emit("callAccepted", data.signal)
-	})
-})
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal);
+    });
+});
 
-server.listen(5000, () => console.log("server is running on port 5000"))
+server.listen(5000, () => console.log("server is running on port 5000"));
